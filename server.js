1fc21451b4d8b6f61fe332c404e930cd9a8f958f
KasturiKannada <48135571+KasturiKannada@users.noreply.github.com>
2019-03-04 13:08:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ app.use(express.static("TLG")); // myApp will be the same folder name.
 app.get('/', function (req, res,next) {
  res.redirect('/'); 
 });
-app.listen(3000, 'https://kasturikannada.herokuapp.com');
+app.listen(process.env.PORT || 8080);
+// app.listen(3000, 'https://kasturikannada.herokuapp.com');
 // app.listen(3000,() => {
 // console.log("Server running at port on port 3000");
 // });
