53e6c83df9817abdce5b7f2ffa635b6908ec8549
vignesh06 <vigneshsn@panainfotech.com>
2019-03-04 15:48:37 +0530

    fourth commit

--- TLG/js/project/toolListController.js
+++ TLG/js/project/toolListController.js
@@ -100,7 +100,7 @@ $scope.submit=function() {
     $.ajax({
       type: 'POST',
       // url: "http://127.0.0.1:5000/store_file?language="+ base64data +"&topic="+$scope.topic+"&index="+$scope.index,
-      url: "http://127.0.0.1:5000/store_file",
+      url: "https://audiosave.herokuapp.com/store_file",
       data: JSON.stringify({'language':base64data,'topic':$scope.topic,'index':$scope.index}),
       cache: false,
       processData: false,
