7403bfbd57710d10506cd87cdbbfd655be488873
KasturiKannada <48135571+KasturiKannada@users.noreply.github.com>
2019-03-04 13:59:18 +0530

    Update toolListController.js

--- TLG/js/project/toolListController.js
+++ TLG/js/project/toolListController.js
@@ -99,7 +99,7 @@ $scope.submit=function() {
     // fd.append('file', base64data, 'audio.mp3');
     $.ajax({
       type: 'POST',
-      url: "http://127.0.0.1:5000/store_file?language="+ base64data +"&topic="+$scope.topic+"&index="+$scope.index,
+      url: "https://audiosave.herokuapp.com/store_file?language="+ base64data +"&topic="+$scope.topic+"&index="+$scope.index,
       // data: {"language":"wewevwe"},
       cache: false,
       processData: false,
