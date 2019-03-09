a157726d8612b7e1f43283aa8da1c7eea87c135a
Akhilesh <akhilesh.rocking@gmail.com>
2019-03-09 08:53:31 +0530

    check drive api

--- app.py
+++ app.py
@@ -1,44 +1,72 @@
 from flask import Flask, jsonify
-from flask import Flask, render_template, request, redirect, Response,abort
+from flask import Flask, render_template, request, redirect, Response, abort
 from flask_cors import CORS, cross_origin
 import json
 import base64
 import os
 import os.path
 import time
+from __future__ import print_function
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-import httplib2
-from apiclient import discovery
+SCOPES = ["https://www.googleapis.com/auth/drive.metadata.readonly"]
 
-scope = ['https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_dict(json.loads(os.environ.get('CREDENTIALS')), scope)
-client = gspread.authorize(creds)
-
-http = creds.authorize(httplib2.Http())
-service = discovery.build('drive', 'v3', http=http, cache_discovery=False)
+creds = None
+json_creds = os.getenv("GOOGLE_SHEETS_CREDS_JSON")
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/store_file', methods=['POST'])
+
+@app.route("/store_file", methods=["POST"])
 def store_file():
     OUTPUT_FOLDER = "Audio_Files"
-    language = request.json['language']    
-    topic = request.json['topic']  
-    index=request.json['index']
+    language = request.json["language"]
+    topic = request.json["topic"]
+    index = request.json["index"]
 
-    language=language.replace('data:audio/x-mpeg-3;base64,', '')
-    d = base64.b64decode(language,' /')
+    language = language.replace("data:audio/x-mpeg-3;base64,", "")
+    d = base64.b64decode(language, " /")
+
+    if os.path.exists("token.pickle"):
+        with open("token.pickle", "rb") as token:
+            creds = pickle.load(token)
+
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                json.loads(os.environ.get(json_creds), SCOPES)
+            )
+            creds = flow.run_local_server()
+        # Save the credentials for the next run
+        with open("token.pickle", "wb") as token:
+            pickle.dump(creds, token)
+
+    service = build("drive", "v3", credentials=creds)
+    results = (
+        service.files()
+        .list(pageSize=10, fields="nextPageToken, files(id, name)")
+        .execute()
+    )
+
+    items = results.get("files", [])
+    if not items:
+        print("No files found.")
+    else:
+        print("Files:")
+        for item in items:
+            print(u"{0} ({1})".format(item["name"], item["id"]))
 
     timestr = time.strftime("%Y%m%d-%H%M%S")
-    service.files().copy(fileId=mp3,
-                         body={{"parents": [topic],
-                                "name": ix,
-                                "kind": "drive#fileLink"
-                                }).execute()
+
     return jsonify({"success": index})
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True, port=33507)
